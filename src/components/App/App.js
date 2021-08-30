4059ff84f4d7082b0b89500b4a974ace50e19dc1
pcmueller <pmuellerleile@gmail.com>
2021-08-29 19:22:58 -0600

    Add App.js page load functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,27 @@ import Details from '../Details/Details';
 
 const App = () => {
 
+  const [ movies, setMovies ] = useState([]);
+  const [ error, setError ] = useState('');
+
+  useEffect(() => {
+    getAllMovies();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getAllMovies = () => {
+    fetchAllMovies()
+      .then(response => {
+        if (response.data) {
+          cleanMovieData(response.data);
+        }
+      })
+      .catch(error => {
+        setError('Sorry, we\'re unable to load the page at the moment.');
+        console.log(error);
+    })
+  }
+
   return (
     <Router>
       <Route>

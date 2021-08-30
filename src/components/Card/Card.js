0db8190dfc1573ae1211583704ae02c359e35333
pcmueller <pmuellerleile@gmail.com>
2021-08-29 19:02:18 -0600

    Build and connect initial Library, Card JSX

---
+++ src/components/Card/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = () => {
+
+  return (
+    <article className='movie-card'>
+      MOVIE TITLE
+      <img />
+    </article>
+  )
+}
+
+export default Card;
+
---
+++ src/components/Library/Library.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+import Card from '../Card/Card';
+
+const Library = () => {
+
+  return (
+    <main className='library'>
+      <Card></Card>
+    </main>
+  )
+}
+
+export default Library;

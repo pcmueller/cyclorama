e3157c3faa088bd63c3017bf74e112f2cc769c4f
pcmueller <pmuellerleile@gmail.com>
2021-08-29 18:38:06 -0600

    Configure index.js and create components directory

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { Route, Redirect, BrowserRouter as Router } from 'react-router-dom';
+
+const App = () => {
+
+  return (
+      <div className='app'>
+
+      </div>
+  );
+}
+
+export default App;

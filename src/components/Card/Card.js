ec52fe382747a0f252153cd59f5ddf82bceb0eee
pcmueller <pmuellerleile@gmail.com>
2021-08-29 19:42:43 -0600

    Convert poster images into NavLinks

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Card = ({ id, title }) => {
 
   return (
-    <article className='movie-card grow'>
-      <img 
-        id={id} 
-        alt={`${title} poster`}
-        src={`moviePosterImages/${id}.jpeg`}
-        onError={(e)=>{e.target.src="moviePosterImages/defaultImage.jpeg"}}
-      />
-    </article>
+    <Link to={`/${id}`} className='link-container' title={title}>
+      <article className='movie-card grow'>
+        <img 
+          id={id} 
+          alt={`${title} poster`}
+          src={`moviePosterImages/${id}.jpeg`}
+          onError={(e)=>{e.target.src="moviePosterImages/defaultImage.jpeg"}}
+        />
+      </article>
+    </Link>
   )
 }
 
